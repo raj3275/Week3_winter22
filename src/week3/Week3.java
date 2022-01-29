d07b247e146650bf9175da20b5d01c5dbfefa7b0
raj3275 <pate3275@gmail.com>
2022-01-29 17:54:16 -0500

    version 2

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -13,7 +13,7 @@ public class Week3 {
 
     /**
      * @param args the command line arguments
-     * Week 3 changes
+     * week 3 changes
      */
     public static void main(String[] args) {
         System.out.println("Learning Github");
